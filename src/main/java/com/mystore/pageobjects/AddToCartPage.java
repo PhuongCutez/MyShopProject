2e29ac9b90afa34ca92bb4f212523fed8ec5a1d4
BichPhuong <21130488@st.hcmuaf.edu.vn>
2024-12-10 23:37:11 +0700

    add cart page

---
+++ src/main/java/com/mystore/pageobjects/AddToCartPage.java
@@ -0,0 +1,56 @@
+
+package com.mystore.pageobjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.mystore.actiondriver.Action;
+import com.mystore.base.BaseClass;
+
+public class AddToCartPage extends BaseClass {
+
+    Action action= new Action();
+
+    @FindBy(name="group_1")
+    private WebElement size;
+
+    @FindBy(id="quantity_wanted")
+    private WebElement quantity;
+
+
+    @FindBy(xpath = "//*[@id=\"add-to-cart-or-refresh\"]/div[2]/div/div[2]/button")
+    private WebElement addToCartBtn;
+
+    @FindBy(xpath="//*[@id=\"myModalLabel\"]/i")
+    private WebElement addToCartMessag;
+
+    @FindBy(xpath="//*[@id=\"blockcart-modal\"]/div/div/div[2]/div/div[2]/div/div/a")
+    private WebElement proceedToCheckOutBtn;
+
+    public AddToCartPage() {
+        PageFactory.initElements(driver, this);
+    }
+    public void selectSize(String size1) throws Throwable {
+        action.selectByVisibleText(size1, size);
+    }
+    public void enterQuantity(String quantity1) throws Throwable {
+        action.type(quantity, quantity1);
+    }
+
+    public void clickOnAddToCart() throws Throwable {
+        action.click(driver, addToCartBtn);
+    }
+
+    public boolean validateAddtoCart() throws Throwable {
+        action.fluentWait(driver, addToCartMessag, 10);
+        return action.isDisplayed(driver, addToCartMessag);
+    }
+
+    public OrderPage clickOnCheckOut() throws Throwable {
+        action.fluentWait(driver, proceedToCheckOutBtn, 10);
+        action.JSClick(driver, proceedToCheckOutBtn);
+        return new OrderPage();
+    }
+
+}
