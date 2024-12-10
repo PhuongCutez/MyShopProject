6f3662cc559ccf5e00a994a56510154212b50799
BichPhuong <21130488@st.hcmuaf.edu.vn>
2024-12-10 14:59:51 +0700

    add create account

---
+++ src/main/java/com/mystore/pageobjects/AccountCreationPage.java
@@ -0,0 +1,86 @@
+/**
+ *
+ */
+package com.mystore.pageobjects;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.mystore.actiondriver.Action;
+import com.mystore.base.BaseClass;
+
+/**
+ * @author Hitendra
+ *
+ */
+public class AccountCreationPage extends BaseClass {
+
+    Action action= new Action();
+
+    @FindBy(xpath = "//h1[text()='Create an account']")
+    private WebElement formTitle;
+
+    @FindBy(id = "field-id_gender-1")
+    private WebElement mr;
+
+    @FindBy(id = "field-id_gender-2")
+    private WebElement mrs;
+
+    @FindBy(name = "firstname")
+    private WebElement firstName;
+
+    @FindBy(name = "lastname")
+    private WebElement lastName;
+
+    @FindBy(id = "field-email")
+    private WebElement email;
+    @FindBy(id = "field-password")
+    private WebElement passWord;
+
+    @FindBy(id = "field-birthday")
+    private WebElement birthDay;
+    @FindBy(name = "optin")
+    private WebElement receiveOffers;
+    @FindBy(name = "psgdpr")
+    private WebElement signUpNewsLetter;
+    @FindBy(name = "newsletter")
+    private WebElement privatePolicy;
+    @FindBy(xpath = "//buttion[text()='Save']")
+    private WebElement saveBtn;
+
+    public AccountCreationPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void createAccount(String gender,String fName,
+                              String lName,
+                              String emails,
+                              String passwd,
+                              String birth) throws Throwable {
+
+        if(gender.equalsIgnoreCase("Mr")) {
+            action.click(driver, mr);
+        } else {
+            action.click(driver, mrs);
+        }
+        action.type(firstName, fName);
+        action.type(lastName, lName);
+        action.type(email, emails);
+        action.type(passWord, passwd);
+        action.type(birthDay, birth);
+        action.click(driver, receiveOffers);
+        action.click(driver, signUpNewsLetter);
+        action.click(driver, privatePolicy);
+
+
+    }
+
+    public HomePage validateRegistration() throws Throwable {
+        saveBtn.click();
+        return new HomePage();
+    }
+
+    public boolean validateAcountCreatePage() throws Throwable {
+        return action.isDisplayed(driver, formTitle);
+    }
+
+}
