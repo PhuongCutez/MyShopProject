3033c2f1026e3b8251e88300879b77c24a3dbbb7
BichPhuong <21130488@st.hcmuaf.edu.vn>
2024-12-25 23:42:32 +0700

    add log for search result

--- src/test/java/com/mystore/testcases/SearchResultPageTest.java
+++ src/test/java/com/mystore/testcases/SearchResultPageTest.java
@@ -1,32 +1,35 @@
 package com.mystore.testcases;
 
 import com.mystore.base.BaseClass;
+import com.mystore.dataprovider.DataProviders;
 import com.mystore.pageobjects.IndexPage;
 import com.mystore.pageobjects.SearchResultPage;
+import com.mystore.utility.Log;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class SearchResultPageTest extends BaseClass {
     private IndexPage indexPage;
     private SearchResultPage searchResultPage;
     @Parameters("browser")
     @BeforeMethod
-    public void setup(String browser){
+    public void setup(@Optional("chrome") String browser){
         launchApp(browser);
     }
     @AfterMethod
     public void tearDown() {
         getDriver().quit();
     }
-    @Test
+    @Test(dataProvider = "searchProduct", dataProviderClass = DataProviders.class)
     public void productAvailabilityTest(String productName) throws Throwable {
+        Log.startTestCase("productAvailabilityTest");
        indexPage = new IndexPage();
+       getDriver().switchTo().frame("framelive");
          searchResultPage = indexPage.searchProduct(productName);
+         Thread.sleep(3000);
             boolean result = searchResultPage.isProductAvailable();
         Assert.assertTrue(result);
+        Log.endTestCase("productAvailabilityTest");
 
     }
 }
