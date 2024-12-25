49a22197e28a6fa1be09883e6d1ac55a03106071
BichPhuong <21130488@st.hcmuaf.edu.vn>
2024-12-25 23:48:16 +0700

    update base class

--- src/main/java/com/mystore/base/BaseClass.java
+++ src/main/java/com/mystore/base/BaseClass.java
@@ -1,57 +1,76 @@
 package com.mystore.base;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.BeforeSuite;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
     public static Properties prop;
+
+    // Declare ThreadLocal Driver
     public static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
-    @BeforeSuite
+
+    //loadConfig method is to load the configuration
+    @BeforeSuite(groups = { "Smoke", "Sanity", "Regression" })
     public void loadConfig() {
+//        ExtentManager.setExtent();
+        Configurator.initialize(null, "log4j.xml");
+
         try {
             prop = new Properties();
-            FileInputStream ip = new FileInputStream(System.getProperty("user.dir") + "\\Configuration\\Config.properties");
+            FileInputStream ip = new FileInputStream(
+                    System.getProperty("user.dir") + "\\Configuration\\config.properties");
             prop.load(ip);
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public static WebDriver getDriver() {
+        // Get Driver from threadLocalmap
         return driver.get();
     }
+
     public void launchApp(String browserName) {
+        // String browserName = prop.getProperty("browser");
         if (browserName.equalsIgnoreCase("Chrome")) {
             WebDriverManager.chromedriver().setup();
+            // Set Browser to ThreadLocalMap
             driver.set(new ChromeDriver());
-        }else if(browserName.equalsIgnoreCase("Firefox")) {
+        } else if (browserName.equalsIgnoreCase("FireFox")) {
             WebDriverManager.firefoxdriver().setup();
             driver.set(new FirefoxDriver());
-        }else if(browserName.equalsIgnoreCase("IE")) {
+        } else if (browserName.equalsIgnoreCase("IE")) {
             WebDriverManager.iedriver().setup();
             driver.set(new InternetExplorerDriver());
         }
+        //Maximize the screen
         getDriver().manage().window().maximize();
-//        getDriver().manage().deleteAllCookies();
-        getDriver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//        getDriver().manage().timeouts().implicitlyWait(Integer.parseInt(prop.getProperty("implicitWait")), TimeUnit.SECONDS);
-//        getDriver().manage().timeouts().pageLoadTimeout(Integer.parseInt(prop.getProperty("pageLoadTimeOut")), TimeUnit.SECONDS);
-
+        getDriver().manage().deleteAllCookies();
+        getDriver().manage().timeouts().implicitlyWait
+                (Integer.parseInt(prop.getProperty("implicitWait")),TimeUnit.SECONDS);
+        //PageLoad TimeOuts
+        getDriver().manage().timeouts().pageLoadTimeout(Integer.parseInt(prop.getProperty("pageLoadTimeOut")),TimeUnit.SECONDS);
         getDriver().get(prop.getProperty("url"));
     }
-//    @AfterSuite
-//    public void afterSuite() {
-//        getDriver().quit();
-//    }
+
+    @AfterSuite(groups = { "Smoke", "Regression","Sanity" })
+    public void afterSuite() {
+//        ExtentManager.endReport();
     }
+}
