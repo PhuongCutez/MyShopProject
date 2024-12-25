00234e41c275ce20a7f1bfc88a86293f081c26ad
BichPhuong <21130488@st.hcmuaf.edu.vn>
2024-12-25 23:44:03 +0700

    add log for order page test

--- src/test/java/com/mystore/testcases/OrderPageTest.java
+++ src/test/java/com/mystore/testcases/OrderPageTest.java
@@ -1,15 +1,14 @@
 package com.mystore.testcases;
 
 import com.mystore.base.BaseClass;
+import com.mystore.dataprovider.DataProviders;
 import com.mystore.pageobjects.AddToCartPage;
 import com.mystore.pageobjects.IndexPage;
 import com.mystore.pageobjects.OrderPage;
 import com.mystore.pageobjects.SearchResultPage;
+import com.mystore.utility.Log;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class OrderPageTest extends BaseClass {
     private IndexPage indexPage;
@@ -18,26 +17,33 @@ public class OrderPageTest extends BaseClass {
     private OrderPage orderPage;
     @Parameters("browser")
     @BeforeMethod
-    public void setup(@org.testng.annotations.Optional("browser") String browser){
+    public void setup(@Optional("chrome") String browser){
         launchApp(browser);
     }
     @AfterMethod
     public void tearDown() {
         getDriver().quit();
     }
-    @Test
-    public void verifyTotalPrice(String productName, String qty, String size) throws Throwable {
+    @Test(dataProvider = "getProduct", dataProviderClass = DataProviders.class)
+    public void verifyTotalPrice(String productName, String size, String qty) throws Throwable {
+        Log.startTestCase("verifyTotalPrice");
         indexPage = new IndexPage();
+        getDriver().switchTo().frame("framelive");
         searchResultPage = indexPage.searchProduct(productName);
         addToCartPage = searchResultPage.clickOnProduct();
         addToCartPage.selectSize(size);
         addToCartPage.enterQuantity(qty);
         addToCartPage.clickOnAddToCart();
+        Thread.sleep(2000);
         orderPage = addToCartPage.clickOnCheckOut();
+        Thread.sleep(2000);
         Double unitPrice = orderPage.getUnitPrice();
+        Thread.sleep(2000);
         Double totalPrice = orderPage.getTotalPrice();
-        Double totalExpectedPrice = (unitPrice*(Double.parseDouble(qty))) + 2;
-        Assert.assertEquals(totalPrice, totalExpectedPrice);
+        Thread.sleep(2000);
+        Double totalExpectedPrice = (unitPrice*(Double.parseDouble(qty))) + 0.02;
+        Assert.assertEquals(totalPrice, totalExpectedPrice, 0.1);
+        Log.endTestCase("verifyTotalPrice");
     }
 
 }
