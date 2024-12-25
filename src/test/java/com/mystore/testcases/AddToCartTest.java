75e67efc0a0fd8836a7eaa1f3a01df7526dabe0b
BichPhuong <21130488@st.hcmuaf.edu.vn>
2024-12-25 23:47:52 +0700

    add log for cart page

--- src/test/java/com/mystore/testcases/AddToCartTest.java
+++ src/test/java/com/mystore/testcases/AddToCartTest.java
@@ -1,34 +1,44 @@
 package com.mystore.testcases;
 
 import com.mystore.base.BaseClass;
+import com.mystore.dataprovider.DataProviders;
 import com.mystore.pageobjects.AddToCartPage;
 import com.mystore.pageobjects.IndexPage;
 import com.mystore.pageobjects.SearchResultPage;
+import com.mystore.utility.Log;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-public class AddToCartPageTest extends BaseClass {
+public class AddToCartTest extends BaseClass {
     private IndexPage indexPage;
     private SearchResultPage searchResultPage;
     private AddToCartPage addToCartPage;
     @Parameters("browser")
     @BeforeMethod
-    public void setup(@Optional("browser") String browser){
+    public void setup(@Optional("chrome") String browser){
         launchApp(browser);
     }
     @AfterMethod
     public void tearDown() {
         getDriver().quit();
     }
-    @Test
+
+    @Test(dataProvider = "getProduct", dataProviderClass = DataProviders.class)
     public void addToCartTest(String productName, String size, String qty) throws Throwable{
+        Log.startTestCase("addToCartTest");
         indexPage = new IndexPage();
+        getDriver().switchTo().frame("framelive");
         searchResultPage = indexPage.searchProduct(productName);
+        Thread.sleep(3000);
         addToCartPage = searchResultPage.clickOnProduct();
-        addToCartPage.selectSize(size);
+        Thread.sleep(3000);
         addToCartPage.enterQuantity(qty);
+        Thread.sleep(3000);
+        addToCartPage.selectSize(size);
+        Thread.sleep(3000);
         addToCartPage.clickOnAddToCart();
         boolean result = addToCartPage.validateAddtoCart();
         Assert.assertTrue(result);
+        Log.endTestCase("addToCartTest");
     }
 }
